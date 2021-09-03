9668d40595965645eebf794cfeaba51da9aa3acf
Sam Atkins <samatkins@hey.com>
2021-09-03 18:03:11 +0100

    update docs

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # ftb - a football CLI
 
+## Install
+
+Get an API token from [www.football-data.org](https://www.football-data.org/) and add to the config file in `$HOME/.config/ftb/config.yaml`.
+
 ```bash
-go install
+# run the install command
+make build
+
+# confirm the binary is installed and check the help menu for available commands
 ftb --help
 ```
-
-## Football API
-
-Uses this API, requires a token.
-
-https://www.football-data.org/
-
-https://www.football-data.org/documentation/api
--- cmd/root.go
+++ cmd/root.go
@@ -19,13 +19,8 @@ var debug bool
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ftb",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "ftb is a CLI tool to get football tables, scorers and matches.",
+	Long:  `ftb is a CLI tool to get football tables, scorers and matches.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },

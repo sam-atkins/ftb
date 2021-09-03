275b0c52651e773e4a7e5a50f81cccdc321a7310
Sam Atkins <samatkins@hey.com>
2021-09-03 17:56:11 +0100

    set kick off times to local

--- cmd/matches.go
+++ cmd/matches.go
@@ -44,7 +44,7 @@ ftb matches -l BL1
 		for _, v := range response.Body.Matches {
 			if v.Season.CurrentMatchday == v.Matchday {
 				rows = append(rows, []string{
-					fmt.Sprint(v.UtcDate.Format("2006 January 02 15:04")),
+					fmt.Sprint(v.UtcDate.Local().Format("2006 January 02 15:04")),
 					v.HomeTeam.Name,
 					v.AwayTeam.Name,
 				})

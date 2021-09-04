d169a10e20951257039b084d4ea283f6db6c5883
Sam Atkins <samatkins@hey.com>
2021-09-04 16:06:55 +0100

    refactor results command
    shows league or club results

--- cmd/results.go
+++ cmd/results.go
@@ -6,6 +6,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/sam-atkins/ftb/api"
 	"github.com/sam-atkins/ftb/writer"
@@ -15,48 +16,101 @@ import (
 // resultsCmd represents the results command
 var resultsCmd = &cobra.Command{
 	Use:   "results",
-	Short: "Shows the results for the previous match day",
-	Long: `Shows the results for the previous match day.
-For example:
+	Short: "Shows football match results",
+	Long: `Shows football match results for a league's previous match day or for a club.
+For example, to show league results:
 
 ftb results --league PL
 ftb results -l BL1
+
+For example, to show results for a club:
+
+ftb results --team FCB
+ftb results -t LIV
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		league, _ := cmd.Flags().GetString("league")
-		if league == "" {
-			// TODO(sam) add default league to config
-			league = "BL1"
+		if league != "" {
+			resultsByLeague(league)
+			return
 		}
 
-		client := api.Client{}
-		endpoint := fmt.Sprintf("competitions/%s/matches", league)
-
-		response, responseErr := client.GetMatches(endpoint)
-		if responseErr != nil {
-			fmt.Printf("Something went wrong with the request %s", responseErr)
+		team, _ := cmd.Flags().GetString("team")
+		if team != "" {
+			resultsByTeam(team)
+			return
 		}
 
-		fmt.Printf("Results from the %v\n", response.Body.Competition.Name)
-
-		header := []string{"Date", "Home", "", "", "Away"}
-		var rows [][]string
-		for _, v := range response.Body.Matches {
-			if v.Season.CurrentMatchday-1 == v.Matchday {
-				rows = append(rows, []string{
-					fmt.Sprint(v.UtcDate.Format("2006 January 02")),
-					v.HomeTeam.Name,
-					fmt.Sprint(v.Score.FullTime.HomeTeam),
-					fmt.Sprint(v.Score.FullTime.AwayTeam),
-					v.AwayTeam.Name,
-				})
-			}
+		fmt.Print("No flag provided. Check the below help menu for options.\n\n")
+		helpErr := cmd.Help()
+		if helpErr != nil {
+			os.Exit(1)
 		}
-		writer.Table(header, rows)
+		os.Exit(1)
 	},
+}
+
+func resultsByLeague(league string) {
+	client := api.Client{}
+	endpoint := fmt.Sprintf("competitions/%s/matches", league)
+
+	response, responseErr := client.GetMatches(endpoint)
+	if responseErr != nil {
+		fmt.Printf("Something went wrong with the request %s", responseErr)
+	}
+
+	fmt.Printf("Results from the %v\n", response.Body.Competition.Name)
+
+	header := []string{"Date", "Home", "", "", "Away"}
+	var rows [][]string
+	for _, v := range response.Body.Matches {
+		if v.Season.CurrentMatchday-1 == v.Matchday {
+			rows = append(rows, []string{
+				fmt.Sprint(v.UtcDate.Format("2006 January 02")),
+				v.HomeTeam.Name,
+				fmt.Sprint(v.Score.FullTime.HomeTeam),
+				fmt.Sprint(v.Score.FullTime.AwayTeam),
+				v.AwayTeam.Name,
+			})
+		}
+	}
+	writer.Table(header, rows)
+}
+
+func resultsByTeam(teamId string) {
+	// TODO(sam) get team id from team Abbrev e.g. 5 is FCB
+	// https://api.football-data.org/v2/teams/5/matches?status=FINISHED
+	// https://api.football-data.org/v2/teams/%s/matches?status=FINISHED
+
+	client := api.Client{}
+	// endpoint := fmt.Sprintf("competitions/%s/matches", league)
+	endpoint := "teams/5/matches?status=FINISHED"
+
+	response, responseErr := client.GetMatches(endpoint)
+	if responseErr != nil {
+		fmt.Printf("Something went wrong with the request %s", responseErr)
+	}
+
+	// TODO(sam) add var for club name
+	// fmt.Printf("Results for %v\n", )
+	fmt.Println("Results for club")
+
+	header := []string{"Date", "Home", "", "", "Away"}
+	var rows [][]string
+	for _, v := range response.Body.Matches {
+		rows = append(rows, []string{
+			fmt.Sprint(v.UtcDate.Format("2006 January 02")),
+			v.HomeTeam.Name,
+			fmt.Sprint(v.Score.FullTime.HomeTeam),
+			fmt.Sprint(v.Score.FullTime.AwayTeam),
+			v.AwayTeam.Name,
+		})
+	}
+	writer.Table(header, rows)
 }
 
 func init() {
 	rootCmd.AddCommand(resultsCmd)
 	resultsCmd.Flags().StringP("league", "l", "", "The league to show e.g. PL, BL1")
+	resultsCmd.Flags().StringP("team", "t", "", "The team to show results for e.g. FCB, LIV")
 }

bd7ffb213da92489a5488d48d7d4afcbbbfd9c1b
Sam Atkins <samatkins@hey.com>
2021-09-10 11:16:28 +0100

    table with team highlighted

--- broker/table.go
+++ broker/table.go
@@ -6,8 +6,10 @@ package broker
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/sam-atkins/ftb/api"
 	"github.com/sam-atkins/ftb/config"
 	"github.com/sam-atkins/ftb/writer"
@@ -43,13 +45,14 @@ func GetTable(league string) {
 	writer.Table(header, rows)
 }
 
-// GetTable fetches gets the league table and prints to stdout
+// GetTable fetches the league table for the given team
 func GetTableForTeam(teamCode string) {
 	var leagueCode string
+	var teamName string
 	for _, v := range config.TeamConfig {
 		if v.Code == strings.ToUpper(teamCode) {
 			leagueCode = v.LeagueCode
-
+			teamName = v.Name
 		}
 	}
 
@@ -62,21 +65,52 @@ func GetTableForTeam(teamCode string) {
 
 	fmt.Printf("League table: %v\n", response.Body.Competition.Name)
 
-	header := []string{"Pos", "Team", "Played", "Won", "Draw", "Lost", "+", "-", "GD", "Points"}
-	var rows [][]string
-	for _, v := range response.Body.Standings[0].Table {
-		rows = append(rows, []string{
-			fmt.Sprint(v.Position),
-			v.Team.Name,
-			fmt.Sprint(v.PlayedGames),
-			fmt.Sprint(v.Won),
-			fmt.Sprint(v.Draw),
-			fmt.Sprint(v.Lost),
-			fmt.Sprint(v.GoalsFor),
-			fmt.Sprint(v.GoalsAgainst),
-			fmt.Sprint(v.GoalDifference),
-			fmt.Sprint(v.Points),
-		})
+	var teamIndex int
+	var data [][]string
+	for i, v := range response.Body.Standings[0].Table {
+		if v.Team.Name == teamName {
+			teamIndex = i
+		}
+		data = append(data,
+			[]string{
+				fmt.Sprint(v.Position),
+				v.Team.Name,
+				fmt.Sprint(v.PlayedGames),
+				fmt.Sprint(v.Won),
+				fmt.Sprint(v.Draw),
+				fmt.Sprint(v.Lost),
+				fmt.Sprint(v.GoalsFor),
+				fmt.Sprint(v.GoalsAgainst),
+				fmt.Sprint(v.GoalDifference),
+				fmt.Sprint(v.Points),
+			})
 	}
-	writer.Table(header, rows)
+
+	// TODO(sam) move to new function in writer
+	table := tablewriter.NewWriter(os.Stdout)
+	header := []string{"Pos", "Team", "Played", "Won", "Draw", "Lost", "+", "-", "GD", "Points"}
+	table.SetHeader(header)
+	table.SetColumnAlignment([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1})
+
+	for i, row := range data {
+		if i == teamIndex {
+			table.Rich(row, []tablewriter.Colors{
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+				{tablewriter.Bold, tablewriter.FgWhiteColor},
+			})
+			// NOTE: important to continue here otherwise the highlighted team is
+			// appended twice
+			continue
+		}
+		table.Append(row)
+	}
+	table.Render()
 }

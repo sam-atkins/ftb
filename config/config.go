2a5016c6f8fc03e0d1a12e17b01a5b7d25b86ca0
Sam Atkins <samatkins@hey.com>
2022-03-15 21:38:41 +0000

    codes command uses teams config file

--- config/config.go
+++ config/config.go
@@ -2,15 +2,23 @@ package config
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/mitchellh/go-homedir"
+	"github.com/sam-atkins/ftb/writer"
+	"gopkg.in/yaml.v2"
 )
+
+var teamConfigFile = ".config/ftb/teams.yaml"
 
 type leagueData struct {
 	LeagueCode string
 	LeagueName string
+	Country    string
 }
 
 type teamData struct {
@@ -25,21 +33,95 @@ var LeagueConfig = []leagueData{
 	{
 		LeagueCode: "BL1",
 		LeagueName: "1. Bundesliga",
+		Country:    "Germany",
 	},
 	{
 		LeagueCode: "PL",
 		LeagueName: "Premier League",
+		Country:    "England",
 	},
 	{
 		LeagueCode: "PD",
 		LeagueName: "La Liga",
+		Country:    "Spain",
 	},
+}
+
+// GetLeagueCodes returns the leagues and their codes
+func GetLeagueCodes() [][]string {
+	var leagueCodes [][]string
+	for _, v := range LeagueConfig {
+		leagueCodes = append(leagueCodes, []string{
+			v.LeagueName,
+			v.LeagueCode,
+			v.Country,
+		},
+		)
+	}
+	return leagueCodes
+}
+
+type teamConfig []struct {
+	ID   int `yaml:"id"`
+	Area struct {
+		ID   int    `yaml:"id"`
+		Name string `yaml:"name"`
+	} `yaml:"area"`
+	Name        string    `yaml:"name"`
+	Shortname   string    `yaml:"shortname"`
+	Tla         string    `yaml:"tla"`
+	Cresturl    string    `yaml:"cresturl"`
+	Address     string    `yaml:"address"`
+	Phone       string    `yaml:"phone"`
+	Website     string    `yaml:"website"`
+	Email       string    `yaml:"email"`
+	Founded     int       `yaml:"founded"`
+	Clubcolors  string    `yaml:"clubcolors"`
+	Venue       string    `yaml:"venue"`
+	Lastupdated time.Time `yaml:"lastupdated"`
+}
+
+func (c *teamConfig) parse(data []byte) error {
+	return yaml.Unmarshal(data, c)
+}
+
+// GetTeamCodesFromConfig returns the teams and their codes
+func GetTeamCodesFromConfig() [][]string {
+	fileName := GetTeamConfigPath()
+	data, fileErr := ioutil.ReadFile(fileName)
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+
+	var teamCfg teamConfig
+	if parseErr := teamCfg.parse(data); parseErr != nil {
+		log.Fatal(parseErr)
+	}
+
+	var teamCodes [][]string
+	for _, v := range teamCfg {
+		teamCodes = append(teamCodes, []string{
+			v.Name,
+			v.Tla,
+			v.Area.Name,
+		},
+		)
+	}
+	return teamCodes
+}
+
+// CodeNotFound used when the user enters an unknown flag code. It prints the available
+// codes to stdout and exits (1)
+func CodeNotFound() {
+	fmt.Println("Did not recognise that team. These are the available team codes:")
+	header := []string{"Team", "Code"}
+	teamCodes := GetTeamCodesFromConfig()
+	writer.Table(header, teamCodes)
+	os.Exit(1)
 }
 
 // GetTeamConfigPath returns the path to the teams.yml config file
 func GetTeamConfigPath() string {
-	teamConfigFile := ".config/ftb/teams.yaml"
-
 	// Find home directory.
 	home, err := homedir.Dir()
 	if err != nil {
